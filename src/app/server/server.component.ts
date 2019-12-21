867842645764c7d3ab4484e5642b83b8a6126395
guptavikash46 <guptavikash46@gmail.com>
2019-12-21 19:58:05 +0530

    done databinding & added jquery support

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
 })
 
-export class ServerComponent{
+export class ServerComponent implements OnInit{
+    ngOnInit(): void {
+        $(document).ready(function () {
+            $("#addBtn").click(function(){
+                alert("Add button is clicked.");
+            });
+        });
+    }
+    name: String = "Vikas";
+    department: String = "Computer Science";
+    allowServers: boolean = false;
+    
+    constructor(){
+        setTimeout(() => {
+            this.allowServers = true;
+        }, 2000);
+    }
 
+    getName(){
+        return this.name;
+    }
+    getDepartment(){
+        return this.department;
+        }
 }
